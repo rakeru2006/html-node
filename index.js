b855f77b1278c69995b8e036daa45e525ee21d20
rakeru2006 <47589546+rakeru2006@users.noreply.github.com>
2020-12-02 17:35:14 -0600

    fixin readme.md file

--- index.js
+++ index.js
@@ -13,7 +13,20 @@ const promptUser = () => {
           type: 'input',
           name: 'name',
           message: 'What is your name?',
-          default: 'Raquel Ceron ',
+          default: 'Raquel Ceron',
+        },
+
+        {
+          type: 'input',
+          name: 'github',
+          message: 'Enter your GitHub Repository',
+          default : 'https://github.com/rakeru2006',
+        },
+        {
+          type: 'input',
+          name: 'githubPage',
+          message: 'Enter your GitHub page',
+          default : 'https://rakeru2006.github.io/HW2/',
         },
         
         {
@@ -23,65 +36,76 @@ const promptUser = () => {
           mask: '?',
         },
         {
-          type: 'password',
-          message: 'Re-enter password to confirm:',
-          name: 'confirm',
-        },
-        {
           type: 'input',
           message: 'What is the title of New Project?',
           name: 'newProject',
         },
-      
-          
             {
               name: 'contents',
               type: 'checkbox',
               message: 'Select to Enter Table of Contents:',
               choices:[
 
-                '[Description](#contents)',
-                '[License](#License)',
-               '[Installation](#Installation)',
-                '[Usage](#Usage)',
-                '[Contributing](#Contributing)',
-                '[Tests](#Tests)',
-                '[Questions FAQs](#Questions)',
+                '- [Description](#contents)',
+                '- [License](#License)',
+                '- [Usage](#Usage)',
+                '- [Installation](#Installation)',
+                '- [Contributing](#Contributing)',
+                '- [Tests](#Tests)',
+                '- [Questions FAQs](#Questions)',
                 ]
             },
             {
+              name: 'description',
               type: 'input',
               message: 'Enter Description :',
-              name: 'description',
+              default: 'This aplication help to make work live easy',
             },
             {
               name: 'license',
               type: 'rawlist',
               message: 'Select License',
-              choices:[ 'MIT','Apache_2','GPLv3','ISC','BSD',]
-              
+              choices:[ 'MIT','Apache_2','GPLv3','ISC','BSD','GNU','None'] 
+            },
+            {
+              name: 'usage',
+              type: 'checkbox',
+              message: 'Select Usage',
+              choices:[
+              'Innovate',
+              'Learn and experiment',
+              'Contribute',
+              'Work',
+              'Be informed',
+              'View Code ',
+              'To collaborate',]
             },
           {
             type: 'input',
-            message: 'Enter description of Installation instructions ',
+            message: 'Enter description of Installation Requirements :',
             name: 'installation',
+            default :'```lorem ipsum``` ',
+           
           },
-          {
-            name: 'usage',
-            type: 'checkbox',
-            message: 'Select Usage',
-            choices:['Aprender y experimentar',
-                    'Contribuir',
-                    'Trabajo en equipo',
-                    'Estar informado(a)',
-                    'Visor de código',
-                    'Registro de incidencias',]
-          },
-          
           {
             type: 'input',
-            message: 'Enter Contributing',
+            message: 'Enter description of Installation Steps ',
+            name: 'installationSteps',
+            default :'```lorem ipsum``` ',
+           
+          },
+          {
             name: 'contributing',
+            type: 'rawlist',
+            message: 'Enter Contributing',
+            choices:[ 
+            'Report a bug',
+            'Fix errors',
+            'Suggest a new feature',
+            'add features',
+            'Help improve the project',
+            'Other',
+            ]
           },
           {
             type: 'input',
@@ -90,37 +114,9 @@ const promptUser = () => {
           },
           {
             type: 'input',
-            message: 'Enter Question',
+            message: 'Enter Question Was this page helpful?',
             name: 'question',
           },
-         
-          {
-            type: 'input',
-            name: 'location',
-            message: 'Where are you from?',
-          },
-          {
-            type: 'input',
-            name: 'hobby',
-            message: 'What is your favorite hobby?',
-          },
-          {
-            type: 'input',
-            name: 'food',
-            message: 'What is your favorite food?',
-          },
-          {
-            type: 'input',
-            name: 'github',
-            message: 'Enter your GitHub Username',
-          },
-          {
-            type: 'input',
-            name: 'linkedin',
-            message: 'Enter your LinkedIn URL.',
-          },
-       
-   
     ]);
   };
   
@@ -140,48 +136,125 @@ const promptUser = () => {
       );
       
    */
-  const generateHTML = (answers) =>
+  const generateMarkdown = (answers) =>
   `
+  [![Build Status](https://dev.azure.com/vscode/VSCode/_apis/build/status/VS%20Code?branchName=master)](https://aka.ms/vscode-builds)
+[![Feature Requests](https://img.shields.io/github/issues/microsoft/vscode/feature-request.svg)](https://github.com/microsoft/vscode/issues?q=is%3Aopen+is%3Aissue+label%3Afeature-request+sort%3Areactions-%2B1-desc)
+[![Bugs](https://img.shields.io/github/issues/microsoft/vscode/bug.svg)](https://github.com/microsoft/vscode/issues?utf8=✓&q=is%3Aissue+is%3Aopen+label%3Abug)
+[![Gitter](https://img.shields.io/badge/chat-on%20gitter-yellow.svg)](https://gitter.im/Microsoft/vscode)
+
   Command-line application to generate README.md
+  # Title Project ${answers.newProject} 
 ================
 
 Create by ${answers.name}
 
-
-# Project name
-# ${answers.newProject}
-
-
-  ## Table of Contents
-  ***
-
-  ${answers.contents}
+This project  run in this github repository [Github](${answers.github})
+and can visualisate in [Github pages](${answers.githubPage})
 
 
-  ## Description 
+## Table of Contents
+   
+  - [Description](#description)
+  - [License](#license)
+  - [Usage](#usage)
+  - [Installation](#installation)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions FAQs](#questions-faqs)
+
+ -${answers.contents}
+
+
+## Description 
   ***
   General information about this project.
 
   ${answers.description}
   
-  ## License
+## License
   ***
+  
+  
+  This proyect going to used the license:
+  ~~~
+  ${answers.license}
+  ~~~
+  You can read more about this in
+  A list of technologies used within the project:
+  
+  | License Identifier| FSF Free? | OSI Approved? |
+  |:--------------|:-------------:|--------------:|
+  | MIT| Y | Y|
+  | Apache_2.0 | Y | Y|
+  | ISC | Y | Y |
+  | BSD |  | Y |
+  
+  * [MIT](https://spdx.org/licenses/MIT.html)
+  * [Apache_2.0 ](https://spdx.org/licenses/Apache-2.0.html)
+  * [ISC](https://spdx.org/licenses/ISC.html)
+  * [BSD](https://spdx.org/licenses/BSD-1-Clause.html)
+  
+  > Maybe you want to seacrh more of this part.
+  > This is how you do it.SPDX License List
+  > The SPDX License List itself is a list of commonly found licenses and
+  > exceptions used in free and open or collaborative software, data, hardware,
+  > or documentation. The SPDX License List includes a standardized short identifier,
+  > the full name, the license text, and a canonical
+  > permanent URL for each license and exception.
+  > [SPDX License List](https://spdx.org/licenses/)
+    
+    
+## Usage
+  ***
+We are going to create this code to show, help and ${answers.usage}
 
-  ${answers.license} 
+## Installation
+***
+<p align="center">
+  <img alt="VS Code in action" src="https://user-images.githubusercontent.com/1487073/58344409-70473b80-7e0a-11e9-8570-b2efc6f8fa44.png">
+</p>
 
- ${answers.github}</li>
+![algo](https://pbs.twimg.com/profile_images/1053055123193122816/IUwo6l_Q_400x400.jpg)
+
+
+${answers.installation}
+
+Small description step by step that tells how to run and develop the aplication 
+${answers.installationSteps}
+
+## Tests
+***
+
+Demo aplication
+${answers.tests}
+
+
+## Contributing
+***
+You can participate with this project with one of the followin examples: 
+ ${answers.contributing}
+
+
+## Questions FAQs
+***
+${answers.question}
+
+
+
+ 
     `;
   
   // Bonus using async/await and try/catch
   const init = async () => {
-    console.log('hi');
+    console.log('Please answer the following questions:');
     try {
       const answers = await promptUser();
   
-      const html = generateHTML(answers);
+      const markdown = generateMarkdown(answers);
 
   
-      await writeFileAsync('README.md', html);
+      await writeFileAsync('README.md', markdown);
   
       console.log('Successfully wrote to README.md');
     } catch (err) {
